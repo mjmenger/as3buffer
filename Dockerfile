052c96cf82275161026267dc0f01cc57991f0d0f
Mark J Menger <mark.j.menger@gmail.com>
2021-03-25 22:37:03 +0000

    add icontrolrest buffer

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,4 @@ COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
 COPY casc.yaml /var/jenkins_home/casc.yaml
 COPY as3buffer.groovy /usr/local/as3buffer.groovy
+COPY icrestbuffer.groovy /usr/local/icrestbuffer.groovy
---
+++ icrestbuffer.groovy
@@ -0,0 +1,44 @@
+// seedjob.groovy
+pipelineJob('icrestbuffer') {
+    definition {
+        cps {
+            sandbox(true)
+            script("""
+
+// this is an example declarative pipeline that says hello and goodbye
+pipeline {
+    agent any
+    options { 
+        disableConcurrentBuilds() 
+        retry(3)
+    }
+    parameters {
+        string(name: 'ICREST_URI', defaultValue: '', description: 'Provide the iControlREST endpoint.')
+        text(name: 'JSON', defaultValue: '', description: 'The json payload to post to the BIG-IP')
+    }    
+    stages {
+        stage("Echo Request") {
+            steps {
+                echo "AS3 Declaration: " + params.AS3_JSON
+            }
+        }
+        stage("Post the Declaration"){
+            steps {
+                script {
+                    def response = httpRequest url: 'https://'+env.BIGIP_HOST+params.ICREST_URI, 
+                                            httpMode: 'PATCH',
+                                            authentication: 'bigip-creds',
+                                            validResponseCodes: '200:302',
+                                            ignoreSslErrors: true,
+                                            consoleLogResponseBody: true,
+                                            requestBody: params.JSON
+                    println('Status: '+response.status)
+                    println("Content: "+response.content)
+                }
+            }
+        }
+    }
+}""")
+        }
+    }
+}
